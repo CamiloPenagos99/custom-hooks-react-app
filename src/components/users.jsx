b9d4fd035bc190a1cebf1ad788e755c1b4e0b39f
camilo penagos <penagoscamilo99@gmail.com>
2022-01-12 11:39:15 -0500

    feat: get y set, zustand

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,11 +1,15 @@
 import { useStore } from "../zustand/store";
+import { Button } from "@mui/material";
 
 export const Users = () => {
-  const users = useStore((state) => state.users);
+  console.log('Render del componente usuarios')
+  const store = useStore()
   return (
     <>
       <div>Usuarios</div>
-      <div>{JSON.stringify(users)}</div>
+      <div>{JSON.stringify(store.users)}</div>
+      <Button variant="contained" onClick={store.removeUsers}>Limpiar</Button>
+      <Button variant="contained" onClick={store.fillUsers}>Rellenar</Button>
     </>
   );
 };
--- src/zustand/store.js
+++ src/zustand/store.js
@@ -8,5 +8,15 @@ export const useStore = create((set) => ({
     { id: "4", name: "John Chambers", section: "beginners" },
     { id: "5", name: "Joe Johnson", section: "advanced" },
   ],
-  removeUsers: () => set({ bears: 0 }),
+  removeUsers: () => set({ users: [] }),
+  fillUsers: () =>
+    set({
+      users: [
+        { id: "1", name: "Aaron Saunders", section: "advanced" },
+        { id: "2", name: "Andrea Saunders", section: "beginners" },
+        { id: "3", name: "Bill Smith", section: "beginners" },
+        { id: "4", name: "John Chambers", section: "beginners" },
+        { id: "5", name: "Joe Johnson", section: "advanced" },
+      ],
+    }),
 }));
